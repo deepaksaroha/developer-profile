8590ca4dfa454f89aed695aebcfe1188c4967066
deepaksaroha <deepaksaroha1993@gmail.com>
2021-11-09 13:06:23 +0530

    updated profile.js to remove empty profile links

--- static/js/profilepage.js
+++ static/js/profilepage.js
@@ -49,13 +49,13 @@ window.onload = ()=>{
         document.getElementById('intro-line').innerText = res.bio;
 
 
-        linklist('/images/iconfinder_github_317712.png',`https://github.com/${res.github_id}`)
-        linklist('/images/iconfinder_160_Hackerrank_logo_logos_4373234.png',`https://www.hackerrank.com/profile/${res.hackerrank_id}`)
-        linklist('/images/codechef-1324440139527402917_32.png',`https://www.codechef.com/users/${res.codechef_id}`)
-        linklist('/images/iconfinder_2018_social_media_popular_app_logo_linkedin_3225190.png',`https://linkedin.com/in/${res.linkedin_id}`)
-        linklist('/images/iconfinder_Circled_Medium_svg5_5279113.png',`https://medium.com/@${res.medium_id}`)
-        linklist('/images/iconfinder_2018_social_media_popular_app_logo_twitter_3225183.png',`https://twitter.com/${res.twitter_id}`)
-        linklist('/images/email-24px.svg',res.email)
+        res.github_id && linklist('/images/iconfinder_github_317712.png',`https://github.com/${res.github_id}`);
+        res.hackerrank_id && linklist('/images/iconfinder_160_Hackerrank_logo_logos_4373234.png',`https://www.hackerrank.com/profile/${res.hackerrank_id}`)
+        res.codechef_id && linklist('/images/codechef-1324440139527402917_32.png',`https://www.codechef.com/users/${res.codechef_id}`)
+        res.linkedin_id && linklist('/images/iconfinder_2018_social_media_popular_app_logo_linkedin_3225190.png',`https://linkedin.com/in/${res.linkedin_id}`)
+        res.medium_id && linklist('/images/iconfinder_Circled_Medium_svg5_5279113.png',`https://medium.com/@${res.medium_id}`)
+        res.twitter_id && linklist('/images/iconfinder_2018_social_media_popular_app_logo_twitter_3225183.png',`https://twitter.com/${res.twitter_id}`)
+        res.email && linklist('/images/email-24px.svg',res.email)
 
         
         reachlist('/images/location_on-24px.svg', res.location)
